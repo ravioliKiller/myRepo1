246206ea23e544beac82846f0608971bb2c908b4
leguan <leguanw@hotmail.com>
2024-09-12 22:08:51 +0800

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+        for(int i=0; i<5; i++){
+            int x = i;
+            int y = x;
+        }
+        System.out.println( "Hello World!" );
+    }
+}
